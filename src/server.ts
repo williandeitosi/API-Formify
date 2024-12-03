6fb8c7b77ea147de405babafa638f28ae881ba18
williandeitosi <williandeitosi@gmail.com>
2024-12-03 20:59:08 -0300

    refactor:  change express to fastify

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import config from "./config/config";
 
 const PORT = config.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server running on ${address}`);
 });
