455fe9fccc9e0854d98e585033a9af256c5397ce
williandeitosi <williandeitosi@gmail.com>
2024-12-03 21:01:47 -0300

    refactor: change functions to functional paradigm

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,35 @@
-import type { Request, Response } from "express";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { ZodError } from "zod";
+import { userSchema } from "../schemas/auth.schema";
+import { registerService } from "../services/auth.service";
 
-class AuthController {
-  async registerUser(req: Request, res: Response) {
-    res.send("testando controller");
-    return;
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const { email, password } = userSchema.parse(request.body);
+
+    const result = await registerService({ email, password });
+
+    return reply.send(result);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return reply.code(400).send({
+        message: "Erro de validação",
+        errors: error.errors.map((err) => ({
+          field: err.path.join("."),
+          message: err.message,
+        })),
+      });
+    }
+
+    if (error instanceof Error) {
+      return reply.code(400).send({
+        message: error.message,
+      });
+    }
+
+    return reply.code(500).send({ message: "Erro interno do servidor" });
   }
 }
-
-export default AuthController;
