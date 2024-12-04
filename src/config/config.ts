a517e2a0f77a8df511ce9b40b6ea32d7b631f66c
williandeitosi <williandeitosi@gmail.com>
2024-12-03 21:00:20 -0300

    feat: add zod validation in `environment variable`

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,11 @@
-const config = {
+import { z } from "zod";
+
+const configSchema = z.object({
+  PORT: z.coerce.number(),
+});
+
+const config = configSchema.parse({
   PORT: process.env.PORT,
-};
+});
 
 export default config;
