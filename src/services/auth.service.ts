df650e503894b673b48783af0e8d5f85f565d0a9
williandeitosi <williandeitosi@gmail.com>
2024-12-04 22:49:03 -0300

    feat: add logic to gender token and implement loginUser

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,6 @@
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
 import { PrismaUsersRepository } from "../repositories/prisma/prisma-user-repository";
 import type { UsersRepository } from "../repositories/users-repository";
 import type { userInput } from "../schemas/auth.schema";
@@ -19,6 +21,29 @@ export class UserService {
       password: hashPassword,
     });
 
-    return { ...newUser, password: undefined };
+    const token = jwt.sign(
+      {
+        id: newUser.id,
+        email: newUser.email,
+        createdAt: newUser.createAt,
+      },
+      config.JWT_SECRET,
+      { expiresIn: "3h" }
+    );
+
+    return { access_token: token, ...newUser, password: undefined };
+  }
+
+  async loginUser({ email, password }: userInput) {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const userExists = await prismaUsersRepository.findByEmail(email);
+    if (!userExists) {
+      throw new Error("User is not found");
+    }
+
+    const isPasswordValid = compare(password, userExists.password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials");
+    }
   }
 }
