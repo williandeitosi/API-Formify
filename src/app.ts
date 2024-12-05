14c8713477ce2d81b2f64fd963c98c4f433dce84
williandeitosi <williandeitosi@gmail.com>
2024-12-04 22:45:08 -0300

    feat: configure cors to comunicate

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import dotenv from "dotenv";
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import { appRoutes } from "./routes/auth.routes";
 
-dotenv.config();
-
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(appRoutes, { prefix: "/api" });
 
