2c348a0ba22ed2b71367e6ae8a897fe8f2b9fa9c
williandeitosi <williandeitosi@gmail.com>
2024-12-04 22:47:24 -0300

    feat: create login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -15,7 +15,27 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     return reply
       .code(201)
-      .send({ message: "User create successfully!", data: result });
+      .send({ message: "User create successfully!", result });
+  } catch (error) {
+    ErrorHandler(error, reply);
+
+    return reply
+      .code(500)
+      .send({ message: "Erro interno do servidor", error: error });
+  }
+}
+
+export async function login(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const usersRepository = new UserService(prismaUsersRepository);
+
+    const { email, password } = userSchema.parse(request.body);
+
+    const result = await usersRepository.loginUser({ email, password });
+    return reply
+      .code(201)
+      .send({ message: "Login successfully!", data: result });
   } catch (error) {
     ErrorHandler(error, reply);
 
