ae7417b1d3626cc6944472aa1a4d883367e204c0
williandeitosi <williandeitosi@gmail.com>
2024-12-04 22:46:49 -0300

    feat: add `JWT_SECRET`

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
 const configSchema = z.object({
-  PORT: z.coerce.number(),
+  PORT: z.preprocess((value) => Number(value), z.number().positive().int()),
+  JWT_SECRET: z.string().min(1, "JWT_SECRET não pode estar vazio"),
 });
-
 const config = configSchema.parse({
   PORT: process.env.PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
 });
 
 export default config;
