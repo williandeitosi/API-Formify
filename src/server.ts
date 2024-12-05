acbe4c19b85bea969c5aba8c38b80dd24d591498
williandeitosi <williandeitosi@gmail.com>
2024-12-04 22:46:24 -0300

    refactor: change the listen mode and start dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import dotenv from "dotenv";
 import app from "./app";
 import config from "./config/config";
+dotenv.config({ path: "./.env" });
 
 const PORT = config.PORT || 3000;
 
-app.listen({ port: PORT }, (err, address) => {
-  if (err) {
+app
+  .listen({ port: PORT, host: "0.0.0.0" })
+  .then((address) => {
+    console.log(`Server running on ${address}`);
+  })
+  .catch((err) => {
     app.log.error(err);
     process.exit(1);
-  }
-  console.log(`Server running on ${address}`);
-});
+  });
