b4a1fef34d4309aad54f813a426538c769380689
williandeitosi <williandeitosi@gmail.com>
2024-12-12 23:17:46 -0300

    feat: create logic to jwt and return token

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,11 +5,12 @@ import { PrismaUsersRepository } from "../repositories/prisma/prisma-user-reposi
 import type { UsersRepository } from "../repositories/users-repository";
 import type { userInput } from "../schemas/auth.schema";
 
+const prismaUsersRepository = new PrismaUsersRepository();
+
 export class UserService {
   constructor(private usersRepository: UsersRepository) {}
 
   async register({ email, password }: userInput) {
-    const prismaUsersRepository = new PrismaUsersRepository();
     const isExists = await prismaUsersRepository.findByEmail(email);
     if (isExists) {
       throw new Error("User is exists!");
@@ -21,29 +22,27 @@ export class UserService {
       password: hashPassword,
     });
 
-    const token = jwt.sign(
-      {
-        id: newUser.id,
-        email: newUser.email,
-        createdAt: newUser.createAt,
-      },
-      config.JWT_SECRET,
-      { expiresIn: "3h" }
-    );
-
-    return { access_token: token, ...newUser, password: undefined };
+    return { ...newUser, password: undefined };
   }
 
   async loginUser({ email, password }: userInput) {
-    const prismaUsersRepository = new PrismaUsersRepository();
     const userExists = await prismaUsersRepository.findByEmail(email);
     if (!userExists) {
       throw new Error("User is not found");
     }
 
-    const isPasswordValid = compare(password, userExists.password);
+    const isPasswordValid = await compare(password, userExists.password);
     if (!isPasswordValid) {
       throw new Error("Invalid credentials");
     }
+
+    const payload = {
+      id: userExists.id,
+      email: userExists.email,
+      createdAt: userExists.createAt,
+    };
+    const token = jwt.sign(payload, config.JWT_SECRET, { expiresIn: "3h" });
+
+    return { access_token: token };
   }
 }
