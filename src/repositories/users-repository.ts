6b1f3c16fdf08ff75a92e1de5b62e25cfa3e2633
williandeitosi <williandeitosi@gmail.com>
2024-12-30 19:14:03 -0300

    feat: add interface FormRepository

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,6 +1,14 @@
-import type { Prisma, User } from "@prisma/client";
+import type { People, Prisma, User } from "@prisma/client";
 
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
+}
+
+export interface FormRepository {
+  createPerson(data: Prisma.PeopleUncheckedCreateInput): Promise<People>;
+  listPerson(id: number): Promise<People | null>;
+  listPeople(): Promise<People[]>;
+  updatePerson(id: number, data: Prisma.PeopleUpdateInput): Promise<People>;
+  deletePerson(id: number): void;
 }
