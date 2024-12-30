621fd9b324b5707102a9e9dd1060351963da9fe1
williandeitosi <williandeitosi@gmail.com>
2024-12-30 19:12:51 -0300

    feat: add form router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import { appRoutes } from "./routes/auth.routes";
+import { formRoutes } from "./routes/form.routes";
 
 const app = fastify();
 
@@ -9,5 +10,6 @@ app.register(fastifyCors, {
 });
 
 app.register(appRoutes, { prefix: "/api" });
+app.register(formRoutes, { prefix: "/api" });
 
 export default app;
